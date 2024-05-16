91f8661160858fd6f93c178452e2e98a10643076
Filipe <filipemarquesferrao@gmail.com>
2024-05-16 22:02:48 +0100

    API manual mapping

--- Core/Specifications/ProductsWithTypeAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypeAndBrandsSpecification.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Core.Entities;
 
 namespace Core.Specifications
@@ -5,6 +6,12 @@ namespace Core.Specifications
     public class ProductsWithTypeAndBrandsSpecification : BaseSpecification<Product>
     {
         public ProductsWithTypeAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductsWithTypeAndBrandsSpecification(int id) : base(x => x.Id == id)
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
